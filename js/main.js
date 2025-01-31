8c604c9a75e26896e545449062fc4ba57db5d9f6
qlcrhk <105726025+qlcrhk@users.noreply.github.com>
2025-01-31 17:08:09 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,28 +49,28 @@ window.addEventListener("scroll", () => {
   });
 });
 
-// var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-// mapOption = {
-//   center: new kakao.maps.LatLng(37.513384, 127.019785), // 지도의 중심좌표
-//   level: 3, // 지도의 확대 레벨
-//   mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
-// };
+var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+mapOption = {
+  center: new kakao.maps.LatLng(37.513384, 127.019785), // 지도의 중심좌표
+  level: 3, // 지도의 확대 레벨
+  mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
+};
 
-// // 지도를 생성한다
-// var map = new kakao.maps.Map(mapContainer, mapOption);
+// 지도를 생성한다
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// // 지도에 마커를 생성하고 표시한다
-// var marker = new kakao.maps.Marker({
-// position: new kakao.maps.LatLng(37.513333, 127.019774), // 마커의 좌표
-// map: map, // 마커를 표시할 지도 객체
-// });
+// 지도에 마커를 생성하고 표시한다
+var marker = new kakao.maps.Marker({
+position: new kakao.maps.LatLng(37.513333, 127.019774), // 마커의 좌표
+map: map, // 마커를 표시할 지도 객체
+});
 
-// // 커스텀 오버레이를 생성하고 지도에 표시한다
-// var customOverlay = new kakao.maps.CustomOverlay({
-// map: map,
-// content:
-//   '<div style="padding:0 5px;background:#fff;">(주) 팔도</div>',
-// position: new kakao.maps.LatLng(37.513333, 127.019774), // 커스텀 오버레이를 표시할 좌표
-// xAnchor: 0.5, // 컨텐츠의 x 위치
-// yAnchor: 0, // 컨텐츠의 y 위치
-// });
+// 커스텀 오버레이를 생성하고 지도에 표시한다
+var customOverlay = new kakao.maps.CustomOverlay({
+map: map,
+content:
+  '<div style="padding:0 5px;background:#fff;">(주) 팔도</div>',
+position: new kakao.maps.LatLng(37.513333, 127.019774), // 커스텀 오버레이를 표시할 좌표
+xAnchor: 0.5, // 컨텐츠의 x 위치
+yAnchor: 0, // 컨텐츠의 y 위치
+});
